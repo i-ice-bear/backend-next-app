c485d2188cad4d09537623b3bf6fa324e8ed90e5
ice-bear <andydowsen47us@gmail.com>
2022-11-24 17:33:53 +0530

    Remodified image viewport page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { Card } from "@nextui-org/react";
+import { Card, Col, Row, Button, Text, Link } from "@nextui-org/react";
 
 const IndexComponent = () => {
   const [ImageFetch, setImage] = React.useState([]);
@@ -17,17 +17,34 @@ const IndexComponent = () => {
   }, []);
   return (
     <>
-    <div className="row container m-auto">
-
-      {ImageFetch.map((imageItem) => {
-        return (
-          <div className="container col-4">
-            <Card className="my-3">
-              <img src={imageItem.urls.small} />
-            </Card>
-          </div>
-        );
-      })}
+      <div className="row container m-auto">
+        {ImageFetch.map((imageItem) => {
+          return (
+            <div className="container col-4">
+              <Card css={{ w: "100%", height:"600px" }} className="my-3" isHoverable>
+                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+                  <Button auto color="secondary">
+                    <Link
+                      href={imageItem.links.download}
+                      className="text-white"
+                    >
+                      Download Image
+                    </Link>
+                  </Button>
+                </Card.Header>
+                <Card.Body css={{ p: 0 }}>
+                  <Card.Image
+                    src={imageItem.urls.regular}
+                    width="100%"
+                    height="100%"
+                    objectFit="cover"
+                    alt="Card example background"
+                  />
+                </Card.Body>
+              </Card>
+            </div>
+          );
+        })}
       </div>
     </>
   );
